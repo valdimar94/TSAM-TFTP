8ebb488cf018cf151136da00469b61f8c71dab52
valdimar94 <valdimarjonsson94@gmail.com>
2017-09-04 18:44:44 +0000

    Very little progress

--- tsam17/pa1/src/tftpd.c
+++ tsam17/pa1/src/tftpd.c
@@ -7,24 +7,41 @@
 #include <ctype.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int main()
 {
 	int socket_file_descriptor;
 	struct sockaddr_in server, client;
+	char message[512];
 
 	socket_file_descriptor = socket(AF_INET, SOCK_DGRAM, 0);
 
 	if (socket_file_descriptor < 0)
-		printf("An error has occured, failure to create an endpoint for communication.");
+		printf("An error has occured: failure to create an endpoint for communication.\n");
 
 	memset(&server, 0, sizeof(server));
 	server.sin_family = AF_INET;
 	// the htons and htonl convert host byte order to network byte order
 	// sin_port is stored as short, so htons is used
-	server.sin_port = htons(69);
+	// any port below 1024 requires superuser access so not sure what to do to test port 69?
+	server.sin_port = htons(6969);
 	// s_addr is stored as long so htonl is used
 	server.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	printf("Server has stopped!\n");
+	// bind socket to a name with err check
+	bind(socket_file_descriptor, (struct sockaddr *) &server, (socklen_t) sizeof(server));
+
+	while(1){
+		socklen_t len = (socklen_t) sizeof(client);
+        ssize_t n = recvfrom(socket_file_descriptor, message, sizeof(message) - 1,
+                             0, (struct sockaddr *) &client, &len);
+
+        message[n] = '\0';
+        fprintf(stdout, "Received:\n%s\n", message);
+        fflush(stdout);
+
+        sendto(socket_file_descriptor, message, (size_t) n, 0,
+               (struct sockaddr *) &client, len);
+	}
+
 	return 0;
 }
