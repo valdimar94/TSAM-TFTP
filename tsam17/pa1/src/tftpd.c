1c127199e6bba602f5782f6127bf50f019828027
valdimar94 <valdimarjonsson94@gmail.com>
2017-09-06 15:59:08 +0000

    managed to print fill_path

--- tsam17/pa1/src/tftpd.c
+++ tsam17/pa1/src/tftpd.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	int socket_file_descriptor;
 	struct sockaddr_in server, client;
 	char message[512];
-	
+
 	socket_file_descriptor = socket(AF_INET, SOCK_DGRAM, 0);
 
 	if (socket_file_descriptor < 0)
@@ -39,41 +39,37 @@ int main(int argc, char *argv[])
 		printf("Could not bind man.... soorry man");
 		return 0;
 	}
-	
+
 	while(1){
 		socklen_t len = (socklen_t) sizeof(client);
         ssize_t n = recvfrom(socket_file_descriptor, message, sizeof(message) - 1, 0, (struct sockaddr *) &client, &len);
-	
-        //message[n] = '\0';
+
+	//message[n] = '\0';
 	//printf("%d ", message[1]);
-        
-	// the second byte in the array contains the Opcode
+        // the second byte in the array contains the Opcode
 	if (message[1] == RRQ)
-	{	
+	{
 		// We jump to over the first two bytes to get the filename
 		char* file_name = message + 2;
 		// Length of given file name
 		size_t f_n_length = strlen(file_name);
 		printf("%s\n", file_name);
-		
+
 		// Jump over opcode, filename and null terminator to get the mode of transfer.
 		char *mode = message + f_n_length + 3;
 		printf("%s\n", mode);
-		
+
 		// TEST:::
 		size_t argv_length = strlen(argv[2]);
-		
+
 		// Initialize array for data path
 		char full_path[argv_length + f_n_length + 2];
-		
-		strcpy(full_path, "lalala");
-		//strcpy(full_path, file_name);
 
-		printf("%d\n", argv_length);
+		strcpy(full_path, argv[2]);
+		strcpy(full_path + argv_length, "/");
+		strcpy(full_path + argv_length + 1, file_name);
 
 		fprintf(stdout, "Path: %s\n", full_path); fflush(stdout);
-		
-				
 	}
 	else{
 		printf("Ohhhhh, now you fucked uup!");
