598ccea0ada60db9492add6eafd562a8d6f9aad6
valdimar94 <valdimarjonsson94@gmail.com>
2017-09-07 15:44:04 +0000

    Allot of tings wok

--- tsam17/pa1/src/tftpd.c
+++ tsam17/pa1/src/tftpd.c
@@ -8,11 +8,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define RRQ 1
+#define RRQ   1
+#define WRQ   2
+#define DATA  3
+#define ACK   4
+#define ERROR 5
 
 int main(int argc, char *argv[])
 {
-	int port_nr = argc;
 	int socket_file_descriptor;
 	struct sockaddr_in server, client;
 	char message[512];
@@ -26,8 +29,7 @@ int main(int argc, char *argv[])
 	server.sin_family = AF_INET;
 	// the htons and htonl convert host byte order to network byte order
 	// sin_port is stored as short, so htons is used
-	// any port below 1024 requires superuser access so not sure what to do to test port 69?
-	server.sin_port = htons(32001);
+	server.sin_port = htons(atoi(argv[1]));
 	// s_addr is stored as long so htonl is used
 	server.sin_addr.s_addr = htonl(INADDR_ANY);
 
@@ -43,22 +45,7 @@ int main(int argc, char *argv[])
 	{
 		socklen_t len = (socklen_t)sizeof(client);
 		ssize_t n = recvfrom(socket_file_descriptor, message, sizeof(message) - 1, 0, (struct sockaddr *)&client, &len);
-		if (message[1] == 2)
-		{
-			printf("Opcode 2");
-		}
-		if (message[1] == 3)
-		{
-			printf("Opcode 3");
-		}
-		if (message[1] == 4)
-		{
-			printf("Opcode 4");
-		}
-		if (message[1] == 5)
-		{
-			printf("Opcode 5");
-		}
+
 		// the second byte in the array contains the Opcode
 		if (message[1] == RRQ)
 		{
@@ -66,25 +53,30 @@ int main(int argc, char *argv[])
 			char *file_name = message + 2;
 			// Length of given file name
 			size_t f_n_length = strlen(file_name);
-			printf("%s\n", file_name);
+
+			// get client port and ip, print alongside file name requested
+			int client_port = ntohs(client.sin_port);
+			unsigned char *client_ip = (unsigned char *)&client.sin_addr.s_addr;
+
+			printf("file name \"%s", file_name);
+			printf("\" requested from ");
+			printf("%d.%d.%d.%d:", client_ip[0], client_ip[1], client_ip[2], client_ip[3]);
+			printf("%d\n", client_port);
 
 			// Jump over opcode, filename and null terminator to get the mode of transfer.
 			char *mode = message + f_n_length + 3;
-			printf("%s\n", mode);
 
-			// TEST:::
 			size_t argv_length = strlen(argv[2]);
 
 			// Initialize array for data path
 			char full_path[argv_length + f_n_length + 2];
 
+			// construct the full path from client message
 			strcpy(full_path, argv[2]);
 			strcpy(full_path + argv_length, "/");
 			strcpy(full_path + argv_length + 1, file_name);
 
-			fprintf(stdout, "Path: %s\n", full_path);
-			fflush(stdout);
-
+			// open file from location
 			FILE *file;
 			file = fopen(full_path, "r");
 
@@ -99,60 +91,45 @@ int main(int argc, char *argv[])
 			fseek(file, 0, SEEK_SET);
 
 			int curr_file_size;
-			char buffer[512];
 			char full_message[516];
-			memset(buffer, 0, sizeof buffer);
 			int break_next = 0;
 			unsigned short package_nr = 1;
 			while (1)
 			{
 				int retry_attempts = 5;
-				memset(buffer, 0, sizeof buffer);
 				memset(full_message, 0, sizeof full_message);
-				// read from current pointer in file to buffer
-				// char* file_content = buffer;
-				// strcpy(full_message, "0401");
-				// strcpy(full_message + 3, package_nr);
+				memset(message, 0, sizeof message);
+				// construct data pack that will be sent to client
 				full_message[0] = 0;
 				full_message[1] = 3;
 				full_message[2] = package_nr >> 8;
 				full_message[3] = package_nr;
-				curr_file_size = fread(full_message + 4, 1, sizeof buffer, file);
-				// strcpy(full_message + 4, file_content);
-				package_nr++;
+				curr_file_size = fread(full_message + 4, 1, 512, file);
 
-				printf("%d\n", full_message[1]);
-				printf("%d\n", full_message[3]);
-				printf("%s\n", full_message + 4);
-				//for (int i = 4; i < 100; i++)
-				//{
-					//printf("%s", full_message[i]);
-				//}
-				//fprintf(stdout, "Full Message: %s\n", full_message);
-				//fflush(stdout);
-				// file is empty
+				// if file is empty, then last message is sent
 				if (curr_file_size <= 0)
 				{
 					break_next = 1;
 				}
+				// send to and receive from client
 				while (--retry_attempts)
 				{
 					ssize_t return_code = sendto(socket_file_descriptor, full_message, curr_file_size + 4, 0, (struct sockaddr *)&client, len);
-					printf("Return Code: %d\n", return_code);
+
 					if (return_code < 0)
 					{
 						printf("Error sending message");
 					}
-					ssize_t ack_return_code = recvfrom(socket_file_descriptor, message, strlen(message), 0, (struct sockaddr *)&client, &len);
-					// printf("%d\n", ack_return_code);
+					ssize_t ack_return_code = recvfrom(socket_file_descriptor, &message, sizeof(message), 0, (struct sockaddr *)&client, &len);
+
 					if (ack_return_code < 0)
 					{
 						printf("Error with sent message");
 					}
-					else
+					else if (message[1] == ACK)
 					{
-						printf("Printing return opcode:");
-						printf("%d\n", message[1]);
+						// nothing went wrong so pack was successfully sent
+						package_nr++;
 						break;
 					}
 				}
@@ -161,17 +138,20 @@ int main(int argc, char *argv[])
 				{
 					break;
 				}
-				if (curr_file_size < 512)
-				{
-					//memset(full_message, 0, curr_file_size);
-					//break_next = 1;
-				}
 			}
-			printf("Done sending or smt");
 		}
-		else
+		// Handle write request denial
+		else if (message[1] == WRQ)
 		{
-			printf("Ohhhhh, now you fucked uup!");
+			printf("Write requested, denied!\n");
+			char full_message[512];
+			memset(full_message, 0, sizeof full_message);
+			full_message[0] = 0;
+			full_message[1] = 5;
+			full_message[2] = 0;
+			full_message[3] = 3;
+			strcpy(full_message + 4, "Access violation.\0");
+			ssize_t return_code = sendto(socket_file_descriptor, full_message, 512, 0, (struct sockaddr *)&client, len);
 		}
 		fflush(stdout);
 	}
